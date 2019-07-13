93a6009ac09092e809969cae8c445103f035e53f
David den Toom <daviddentoom@gmail.com>
2019-07-13 18:34:57 +0200

    [ci skip] Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"32":"Variable","64":"Function"},"rows":[{"id":0,"kind":32,"name":"INITIAL_CODE","url":"globals.html#initial_code","classes":"tsd-kind-variable tsd-is-not-exported"},{"id":1,"kind":32,"name":"PROP_NAME","url":"globals.html#prop_name","classes":"tsd-kind-variable tsd-is-not-exported"},{"id":2,"kind":64,"name":"StatusCodeComponent","url":"globals.html#statuscodecomponent","classes":"tsd-kind-function tsd-is-not-exported"},{"id":3,"kind":64,"name":"reducePropsToState","url":"globals.html#reducepropstostate","classes":"tsd-kind-function"},{"id":4,"kind":32,"name":"StatusCode","url":"globals.html#statuscode","classes":"tsd-kind-variable"}]};
